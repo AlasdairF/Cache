2efa615c5574d699410e7a2b30c82ad744c0c257
AlasdairF <AlasdairF@users.noreply.github.com>
2015-12-22 20:31:09 +0700

    Changed struct name

--- cache.go
+++ cache.go
@@ -9,8 +9,8 @@ const (
 	sleeptime = time.Minute
 )
 
-caches []*cache
-cacheMutex sync.Mutex
+caches []*bytes
+bytesMutex sync.Mutex
 
 type entry struct {
 	mutex sync.Mutex
@@ -18,7 +18,7 @@ type entry struct {
 	data []byte
 }
 
-type cache struct {
+type bytes struct {
 	vals []*entry
 	mem, max int64
 }
@@ -28,16 +28,16 @@ func init() {
 }
 
 // Creates a new cache
-func New(size int, megabytes int64) *cache {
-	c := &cache{vals: make([]*entry, size), max: megabytes * 1024}
-	cacheMutex.Lock()
+func New(size int, megabytes int64) *bytes {
+	c := &bytes{vals: make([]*entry, size), max: megabytes * 1024}
+	bytesMutex.Lock()
 	caches = append(caches, c)
-	cacheMutex.Unlock()
+	bytesMutex.Unlock()
 	return c
 }
 
 // Gets the slice of bytes assigned to this ID in the cache
-func (c *cache) Get(id int) ([]byte, bool) {
+func (c *bytes) Get(id int) ([]byte, bool) {
 	item := c.vals[id]
 	if item == nil {
 		return nil, false
@@ -51,7 +51,7 @@ func (c *cache) Get(id int) ([]byte, bool) {
 }
 
 // Caches the item, does nothing if it already exists
-func (c *cache) Store(id int, p []byte) {
+func (c *bytes) Store(id int, p []byte) {
 	item := c.vals[id]
 	if item == nil {
 		item = &entry{data: p, lastAccess: time.Now().Unix()}
@@ -61,7 +61,7 @@ func (c *cache) Store(id int, p []byte) {
 }
 
 // Caches the item, replaces it if it already exists
-func (c *cache) Replace(id int, p []byte) {
+func (c *bytes) Replace(id int, p []byte) {
 	tim := time.Now().Unix()
 	item := c.vals[id]
 	if item == nil {
@@ -79,13 +79,13 @@ func (c *cache) Replace(id int, p []byte) {
 }
 
 // Closes the cache, releasing the memory
-func (c *cache) Close() {
+func (c *bytes) Close() {
 	c.vals = nil
 	mem, max = 0
 }
 
 // Removes all entries in the cache last accessed less than this time ago (UNIX Timestamp)
-func (c *cache) Purge(olderThan int64) {
+func (c *bytes) Purge(olderThan int64) {
 	for i, item := range c.vals {
 		if item != nil {
 			item.mutex.Lock()
@@ -102,9 +102,9 @@ func (c *cache) Purge(olderThan int64) {
 func cleaner() {
 	for {
 		time.Sleep(sleeptime)
-		cacheMutex.Lock()
+		bytesMutex.Lock()
 		newCachesSlice := caches
-		cacheMutex.Unlock()
+		bytesMutex.Unlock()
 		for _, c := range newCachesSlice {
 			if atomic.LoadInt64(&c.mem) > atomic.LoadInt64(&c.max) {
 				c.Purge(time.Now().Unix() - 86400) // 1 day ago
